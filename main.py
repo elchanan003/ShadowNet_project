a5ca081558695f1471d42d3f35a64f6d91591389
Elchanan_003 <elchanan003@gmail.com>
2026-06-18 15:12:15 +0300

    add mian file

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from routes.agent_routes import agent_router
 from database.db_connection import db
-
+from routes.agent_routes import agent_router
+from routes.mission_routes import mission_router
+from routes.report_routes import router_report
 
 app = FastAPI()
 app.include_router(agent_router)
+app.include_router(mission_router)
+app.include_router(router_report)
 
 
 @app.on_event("startup")
