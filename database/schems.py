6170b3231af469a915d559e8211c82abe39193cc
Elchanan_003 <elchanan003@gmail.com>
2026-06-18 11:32:53 +0300

    update pydantic

--- database/schems.py
+++ database/schems.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Literal
 
 
 class Agent(BaseModel):
@@ -7,14 +8,14 @@ class Agent(BaseModel):
     is_active          : bool
     completed_missions : int 
     failed_missions    : int  
-    agent_rank         : str = Field(max_length=50)
+    agent_rank         : str = Literal['Junior', 'Senior', 'Commander']
 
 class Mission(BaseModel):
     title             : str = Field(max_length=255)
     description       : str
     location          : str = Field(max_length=255)
-    difficulty        : int
-    importance        : int
+    difficulty        : int = Field(ge=0, le=10)
+    importance        : int = Field(ge=0, le=10)
     status            : str = Field(max_length=50)
     assigned_agent_id : int | None
 
